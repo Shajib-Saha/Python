83015ad33bc5832754ca911ce2622f4fc5d16cb7
Shajib Saha <84149541+Shajib-Saha@users.noreply.github.com>
2025-05-23 19:03:33 +0600

    Add files via upload

---
+++ keyword_arguments.py
@@ -0,0 +1,7 @@
+def greet_user(f_name, l_name):
+    print(f"Hello {f_name} {l_name}!")
+    print("Welcome to this code")
+
+print("Start the code ")
+greet_user(f_name='Shajib', l_name='Saha')
+print('Finish!')
